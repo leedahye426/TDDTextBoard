802fbed63f090b421a331f5611c495334314220c
leedahye426 <9535957@naver.com>
2023-11-08 17:06:25 +0900

    t3_2, pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -12,5 +12,6 @@ public class App {
         System.out.println("=== 명언 앱 ===");
         System.out.print("명언 : ");
         System.out.println("작가 : ");
+        System.out.println("1번 명언이 등록되었습니다.");
     }
 }

e959a4d8946349e480eddebacfd8a7002a33fb3e
leedahye426 <9535957@naver.com>
2023-11-08 16:49:20 +0900

    t3, refactoring

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -10,12 +10,9 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
-    @Test
-    @DisplayName("프로그램 시작 시 \"== 명언 앱 ==\" 출력")
-    void t1() {
-        Scanner scanner = TestUtil.genScanner("""
-                종료
-                """.stripIndent());
+
+    private  String run(String cmd) {
+        Scanner scanner = TestUtil.genScanner(cmd);
 
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
@@ -23,6 +20,15 @@ public class AppTest {
 
         String out = byteArrayOutputStream.toString().trim();
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        return out.trim();
+    }
+    @Test
+    @DisplayName("프로그램 시작 시 \"== 명언 앱 ==\" 출력")
+    void t1() {
+        String out = run("""
+                종료
+                """);
 
         assertThat(out)
                 .contains("== 명언 앱 ==");
@@ -31,34 +37,20 @@ public class AppTest {
     @Test
     @DisplayName("종료")
     void t2() {
-        Scanner scanner = TestUtil.genScanner("""
+        String out = run("""
                 종료
-                """.stripIndent());
-
-        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
-
-        new App(scanner).run();
-
-        String out = byteArrayOutputStream.toString().trim();
-        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+                """);
     }
 
     @Test
     @DisplayName("등록")
     void t3() {
-        Scanner scanner = TestUtil.genScanner("""
+        String out = run("""
                 등록
                 현재를 사랑하라.
                 작자미상
                 종료
-                """.stripIndent());
-
-        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
-
-        new App(scanner).run();
-
-        String out = byteArrayOutputStream.toString().trim();
-        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+                """);
 
         assertThat(out)
                 .contains("명언 :")
